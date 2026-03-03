1167cc48c3898664e4e53884df75547b2c8dfc6e
MegaBrute <mickeymadrigal@yahoo.com>
2026-03-02 23:51:11 -0600

    FMN-251: Add support to upload before and after photos to project

---
+++ Migrations/20260303054301_AddProjectDatesAndPictures.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProjectService.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddProjectDatesAndPictures : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "AfterPictureUrl",
+                table: "Projects",
+                type: "character varying(500)",
+                maxLength: 500,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "BeforePictureUrl",
+                table: "Projects",
+                type: "character varying(500)",
+                maxLength: 500,
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DueDate",
+                table: "Projects",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "StartDate",
+                table: "Projects",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AfterPictureUrl",
+                table: "Projects");
+
+            migrationBuilder.DropColumn(
+                name: "BeforePictureUrl",
+                table: "Projects");
+
+            migrationBuilder.DropColumn(
+                name: "DueDate",
+                table: "Projects");
+
+            migrationBuilder.DropColumn(
+                name: "StartDate",
+                table: "Projects");
+        }
+    }
+}
